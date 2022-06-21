3b6095dc1ddbd5e68cd6c89930a9ef5d2f1cbee5
GD3242 <dulguung578@gmail.com>
2022-06-21 23:32:48 +0800

    Initial commit

---
+++ public/build/routes/healthcheck-FT7RQWOM.js
@@ -0,0 +1,14 @@
+import {
+  __commonJS,
+  init_react
+} from "/build/_shared/chunk-6BO74FWO.js";
+
+// browser-route-module:/Users/dulguun/Documents/projects/remix/blog-tutorial/app/routes/healthcheck.tsx?browser
+var require_healthcheck = __commonJS({
+  "browser-route-module:/Users/dulguun/Documents/projects/remix/blog-tutorial/app/routes/healthcheck.tsx?browser"(exports, module) {
+    init_react();
+    module.exports = {};
+  }
+});
+export default require_healthcheck();
+//# sourceMappingURL=/build/routes/healthcheck-FT7RQWOM.js.map
